c5a68c199fe686d7f8ff9adef57bcfa01c59d6e0
AlexKVal <alexkval@gmail.com>
2015-03-23 20:31:51 +0200

    use bl for 8

--- 8_http_collect.js
+++ 8_http_collect.js
@@ -1,16 +1,10 @@
 var http = require('http');
-
-var fullString = '';
+var bl = require('bl');
 
 http.get(process.argv[2], function (response) {
-  // console.log('STATUS:', response.statusCode, http.STATUS_CODES[response.statusCode]);
-
-  response.setEncoding('utf8');
-  response.on('data', function (chunk) {
-    fullString += chunk;
-  });
-  response.on('end', function () {
-    console.log(fullString.length);
-    console.log(fullString);
-  });
+  response.pipe(bl(function (err, data) {
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+  }));
 });
