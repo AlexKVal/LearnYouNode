41906643c8df150ad992132a3afd091b007f6f2c
AlexKVal <alexkval@gmail.com>
2015-03-23 22:56:59 +0200

    11_http_fileserver with 200

--- 11_http_fileserver.js
+++ 11_http_fileserver.js
@@ -5,8 +5,8 @@ var port = process.argv[2];
 var path = process.argv[3];
 
 var srv = http.createServer(function (req, res) {
-  var fileStream = fs.createReadStream(path); //, {encoding: 'utf8'});
-  fileStream.pipe(res);
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  fs.createReadStream(path, {encoding: 'utf8'}).pipe(res);
 });
 
 srv.listen(port);
