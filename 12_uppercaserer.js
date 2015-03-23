126ed212ad74e3aa9cf725f72a20f6021d1a8c37
AlexKVal <alexkval@gmail.com>
2015-03-23 23:18:45 +0200

    12_uppercaserer

---
+++ 12_uppercaserer.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var through2map = require('through2-map');
+
+var port = process.argv[2];
+
+http.createServer(function (req, res) {
+  req.pipe(through2map(function (chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(res);
+}).listen(port);
